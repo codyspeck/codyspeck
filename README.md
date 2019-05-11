59354dceb03b15c59911bd982e825b650d19bd61
Cody <cspeck7@gmail.com>
2019-05-11 18:53:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Hi
+
+I'm Cody and this is my resume project.
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Cody Peck</h1>
+    </div>
+  );
+}
+
+export default App;
