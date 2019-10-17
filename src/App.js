5bed4d85fd23de035e7e8b9918e76bfe849e6d8d
Cody Peck <cpeck@channelsoftware.com>
2019-10-17 12:36:19 -0500

    Retheming to 8-bit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.container}>
-      <h1>Cody Peck</h1>
+      <h1>> Cody Peck</h1>
     </div>
   );
 }
